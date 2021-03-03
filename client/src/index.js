f834e77bfe856fa082c4faa32eba2250553c9ec9
TaraYu <46502584+TaraYu@users.noreply.github.com>
2021-03-03 12:04:42 +0800

    main page

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
 // redux
 import { connect } from "react-redux";
+import "./style/common.css";
+import Header from "../components/Header";
+import Gridsturc from "../components/Gridsturc";
 
 class MainPage extends React.Component {
-    componentDidMount = () => {
-        // extract user
-        const user = this.props.currentUser;
-    }
+  /*componentDidMount = () => {
+    // extract user
+    const user = this.props.currentUser;
+  };*/
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Gridsturc></Gridsturc>
+      </div>
+    );
+  }
 }
 
+export default MainPage;
+/*
 // redux
 const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser,
+  currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProps)(MainPage);
+*/
