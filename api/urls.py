e343315b612172fbd4c7bf2417aa38127557d4c5
UAACC <dongheng@ualberta.ca>
2021-03-03 02:21:10 -0700

    Posts update
    1.Author: make public posts(done)
    2.Author: edit public posts(done)
    3.Author: create post image
    And text (done)
    4.Admin: post image on server(done)

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import UserViewSet, AuthorViewSet, PostViewSet, CommentViewSet, LikeViewSet,UpdateViewSet,DeleteViewSet
-
+from .views import UserViewSet, AuthorViewSet, PostViewSet, CommentViewSet, LikeViewSet,PostDetail,UpdatePost
+from . import views
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
 router.register('authors', AuthorViewSet)
 router.register('posts', PostViewSet)
-router.register('update', UpdateViewSet)
-router.register('delete', DeleteViewSet)
+
 router.register('comments', CommentViewSet)
 router.register('likes', LikeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('<int:pk>/', PostDetail.as_view()),#router?
+    path('edit/<int:pk>/', UpdatePost.as_view()),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,14 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status,generics
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
-from .serializers import UserSerializer, AuthorSerializer, PostSerializer, CommentSerializer, LikeSerializer
+from .serializers import UserSerializer, AuthorSerializer, PostSerializer, CommentSerializer, LikeSerializer,UpdateSerializer
 from .models import Author, Post
 from django.http import HttpResponse
-from rest_framework.generics import(UpdateAPIView, DestroyAPIView)
+
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -27,31 +28,34 @@ class AuthorViewSet(viewsets.ModelViewSet):
         return Response(author, status=status.HTTP_200_OK)
 
 class LikeViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.postobjects.all()
     serializer_class = LikeSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.postobjects.all()
     serializer_class = CommentSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny, )
 
-class UpdateViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny, )
 
-class DeleteViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UpdatePost(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = UpdateSerializer
     permission_classes = (AllowAny, )
+    
+
+    
 
 
 # def send_friend_request(request, userID):
