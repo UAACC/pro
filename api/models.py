479cafb68dfede8fc9eaef10bb23ff0cff960c47
xutong-li <lixutong99@gmail.com>
2021-03-02 19:03:19 -0700

    add friend request

--- api/models.py
+++ api/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+#
+# class User(AbstractUser):
+#     friends = models.ManyToManyField("User", blank=True)
 
 class Author(models.Model):
     username = models.CharField(max_length=50, unique=True)
@@ -14,3 +19,12 @@ class Post(models.Model):
     description = models.CharField(max_length=256, default="")
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
     published = models.DateTimeField(default=timezone.now)
+
+#
+# class Friend_Request(models.Model):
+#     from_user = models.ForeignKey(
+#         User, related_name='from_user', on_delete=models.CASCADE
+#     )
+#     to_user = models.ForeignKey(
+#         User, related_name='to_user', on_delete=models.CASCADE
+#     )
--- api/views.py
+++ api/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, AuthorSerializer, PostSerializer
 from .models import Author, Post
+# from .models import Friend_Request
+from django.http import HttpResponse
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -27,3 +29,28 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny, )
+#
+#
+# def send_friend_request(request, userID):
+#     from_user = request.user
+#     to_user = User.objects.get(id=userID)
+#     friend_request, created = Friend_Request.objects.get_or_create(
+#         from_user = from_user, to_user = to_user
+#     )
+#     if created:
+#         return HttpResponse('Friend request sent')
+#     else:
+#         return HttpResponse('Friend request not accepted')
+#
+#
+# def accept_friend_request(request, requestID):
+#     friend_request = Friend_Request.objects.get(id=requestID)
+#     if friend_request.to_user == request.user:
+#         friend_request.to_user.friends.add(friend_request.from_user)
+#         friend_request.from_user.friends.add(friend_request.to_user)
+#         friend_request.delete()
+#         return HttpResponse('Friend request accepted')
+#     else:
+#         return HttpResponse('Friend request not accepted')
+#
+#
