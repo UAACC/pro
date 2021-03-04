b71bcfe2a4f72db0babc996fed38b4c96ab7f441
UAACC <dongheng@ualberta.ca>
2021-03-04 00:35:59 -0700

    update

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import UserViewSet, AuthorViewSet, CommentViewSet, LikeViewSet,PostList,PostDetail,UpdatePost,PostCreate,DeletePost,#PostSearchList
+from .views import UserViewSet,AuthorViewSet,CommentViewSet,LikeViewSet,PostDetail,UpdatePost,PostCreate,DeletePost,PostList
 from . import views
 
 
