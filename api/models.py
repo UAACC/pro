52bdc7469f40a564bd17d9d0f5e74303feceae6e
UAACC <dongheng@ualberta.ca>
2021-03-04 16:23:07 -0700

    d

--- api/models.py
+++ api/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+import uuid
 
-class Author(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    display_name = models.CharField(max_length=50, null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    github = models.URLField(null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    is_approved = models.BooleanField(default=False)
+
+    
+
 
 options = (('draft','Draft'),
 
